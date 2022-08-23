32459990135ae2a4a98f52c8106bf333fdaf345c
Peac0 <111903159+Peac0@users.noreply.github.com>
2022-08-23 19:17:53 +0200

    Update first_example.py

--- first_example.py
+++ first_example.py
@@ -33,21 +33,5 @@ for row in match_detail['participants']:
     participants_row['item1'] = row['item1']
     participants.append(participants_row)
 df = pd.DataFrame(participants)
-
-# check league's latest version
-latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-# Lets get some champions static information
-static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
-
-# champ static list data to dict for looking up
-champ_dict = {}
-for key in static_champ_list['data']:
-    row = static_champ_list['data'][key]
-    champ_dict[row['key']] = row['id']
-for row in participants:
-    print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
-    row['championName'] = champ_dict[str(row['champion'])]
-
-# print dataframe
-df = pd.DataFrame(participants)
+#print data frame
 print(df)
